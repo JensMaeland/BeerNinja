b0b06efd90a27a8785f98283d62845cee6cf709b
Jens Mæland <jens_mae@hotmail.com>
2021-03-13 13:11:29 +0100

    initial commit

---
+++ BeerNinja/android/src/com/mygdx/beerninja/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.beerninja;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.beerninja.BeerNinja;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new BeerNinja(), config);
+	}
+}
